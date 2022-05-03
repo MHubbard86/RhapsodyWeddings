857a9d6b2999c032bbd54ec53661255bca60d5cc
Matthew Hubbard <50264772+MHubbard86@users.noreply.github.com>
2022-05-03 11:47:30 +0100

    Image improvements
    Image improvements

--- src/components/Images/ImageGallery/ImageGallery.jsx
+++ src/components/Images/ImageGallery/ImageGallery.jsx
@@ -3,6 +3,7 @@ import ImageGallery from "react-image-gallery";
 import "react-image-gallery/styles/css/image-gallery.css";
 import Images from './Images';
 
+
 const Gallery = () => {
   return <ImageGallery items={Images} autoPlay={true} />;
 }
