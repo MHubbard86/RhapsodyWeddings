024e924a6d8b745642850e03a5554d41d3f0ac61
Matthew Hubbard <50264772+MHubbard86@users.noreply.github.com>
2022-05-03 12:18:43 +0100

    Images tweak
    Images tweak

--- src/components/Images/ImageGallery/ImageGallery.jsx
+++ src/components/Images/ImageGallery/ImageGallery.jsx
@@ -3,9 +3,14 @@ import ImageGallery from "react-image-gallery";
 import "react-image-gallery/styles/css/image-gallery.css";
 import Images from './Images';
 
-
 const Gallery = () => {
-  return <ImageGallery items={Images} autoPlay={true} />;
+  return <ImageGallery
+    items={Images}
+    autoPlay={true}
+    showNav={false} 
+    showFullscreenButton={false}
+    showPlayButton={false}
+  />
 }
 
 export default Gallery;
