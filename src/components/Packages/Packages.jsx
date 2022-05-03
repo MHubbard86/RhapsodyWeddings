0c232e2254235b0423ccd55fc9f89c36a19cce05
Matthew Hubbard <50264772+MHubbard86@users.noreply.github.com>
2022-05-03 10:42:00 +0100

    Image Gallery
    Image Gallery

--- src/components/Packages/Packages.jsx
+++ src/components/Packages/Packages.jsx
@@ -1,9 +1,13 @@
 import React from "react";
+import ImageGallery from '../Images/ImageGallery/ImageGallery';
 
 export default function Packages() {
   return (
     <>
-      <h1>Packages</h1>      
+      <h1>Packages</h1>
+      <div style={{ width: '70%', marginTop: '20px' }}>
+        <ImageGallery />
+      </div>    
     </>
   );
 }
