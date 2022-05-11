41939d32780eb9d5fa05c6e7c4f08ca1da239ad7
Matthew Hubbard <50264772+MHubbard86@users.noreply.github.com>
2022-05-11 12:46:16 +0100

    FAQ improvements
    FAQ improvements

---
+++ src/features/FAQ/Questions.js
@@ -0,0 +1,28 @@
+const FAQ = [
+  {
+    question: "How does it work?",
+    answer: "Answer...",
+  },
+  {
+    question: "How do Hair Trails work?",
+    answer: "Answer...",
+  },
+  {
+    question: "Can I have both Hair and Décor?",
+    answer: "Answer...",
+  },
+  {
+    question: "How does Delivery and Collection work?",
+    answer: "Answer...",
+  },
+  {
+    question: "How long in advanced can I book?",
+    answer: "Answer...",
+  },
+  {
+    question: "Can I book my Hair and then add on Décor later?",
+    answer: "Answer...",
+  },
+];
+
+export default FAQ;
